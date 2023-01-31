5a9044eb65a50fffaeda6c31021b61c88d05aa12
LillyIlex <116085080+LillyIlex@users.noreply.github.com>
2023-01-31 14:12:03 +0000

    added click event function for search bar. captured input and created ajax to call the lat and lon from one url to put into the next ajax url, thus returning an object to track name, date and weather conditions. Inputted a local stoarge function to ammend. Also fixed ajax url calls as they weren't previously working.

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,85 @@
+//added date and time from moment.js
 var datetime = null,
-date = null;
+    date = null;
 
 var update = function () {
-date = moment(new Date())
-datetime.html(date.format('dddd, MMMM Do YYYY, h:mm a'));
+    date = moment(new Date())
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm a'));
 };
 $(document).ready(function () {
-datetime = $('#currentDay')
-update();
-setInterval(update, 1000);
+    datetime = $('#currentDay')
+    update();
+    setInterval(update, 1000);
 });
 
 
-var apiKey = "bea3b67283dd3660e8a767a5a906068a"
-var limit = 1
-var city = "manchester"
-
-geoURL = "http://api.openweathermap.org/geo/1.0/direct?q=%7Bcity=" + city + "&limit=" + limit + "&appid=" + apiKey
-
-//var geoURL = "http://api.openweathermap.org/geo/1.0/direct?q=%7Bcity=London&limit=1&appid=" + apiKey
-
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=%7Blat%7D&lon=%7Blon%7D&appid=" + apiKey
+//add event listener for search button
+var searchBtn = $("#search-button")
+var city = $("#search-input:text")
+var search
 
 
-$.ajax({
-    url: geoURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response)
-    var city 
-/* var date = response.
-    var temp = response.
-    var wind = response.0.list.wind
-    var humidity = response.
-    var icon = response.0.weather.icon */
+searchBtn.on("click", function (event) {
+    event.preventDefault()
+    //value of text inputted saved as new var
+    var cityResults = city.val()
+    console.log(cityResults)
+
+    var limit = 3
+    var apiKey = "bea3b67283dd3660e8a767a5a906068a"
+    var geoURL = "https://api.openweathermap.org/geo/1.0/direct?q=%7Bcity=" + cityResults + "&limit=" + limit + "&appid=" + apiKey
+    console.log(geoURL);
+
+    //ajax to fetch URL from api.
+    $.ajax({
+        url: geoURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+
+        var lat = response[0].lat.toFixed(2)
+        var lon = response[0].lon.toFixed(2)
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(queryURL);
+            console.log(response)
+            var cityName = response.city.name;
+            var date = response.list[0].dt_txt;
+            var temp = response.list[0].main.temp;
+            var wind = response.list[0].wind.speed;
+            var humidity = response.list[0].main.humidity;
+            var icon = response.list[0].weather[0].icon;
+            // console.log(icon + date + wind + temp + humidity + cityName);
+        })
+
+
+    })
+
+    //append previous searches into buttons;
+   /* $("#search-button").on("click", function (event) {
+        event.preventDefault();
+        localStorage.setItem("city", cityResults);
+        //for loop?
+        $(function () {
+
+            $("#1").val(localStorage.getItem("city"));
+            $("#2.innerHTML").val(localStorage.getItem("city"));
+            $("#3.innerHTML").val(localStorage.getItem("city"));
+            $("#4.innerHTML").val(localStorage.getItem("city"));
+            $("#5.innerHTML").val(localStorage.getItem("city"));
+
+        });
+    }) */
+
 })
 
 
-$.ajax({
-   url: queryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response)
+//var cityName = $(".card-title")
+// append text input with innerHTML
 
-}) 
+//append previous searches into buttons;
